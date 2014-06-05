c83df83d9c00a3bc49cf7e37e7fc07a42cfdd72c
neillobo <neillobo@outlook.com>
2014-06-04 21:06:16 -0700

    complete basic requirements

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,26 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this._storage = {};
+  this._size = 0;
+  this._head = 0;
 };
 
+Queue.prototype.size = function () {
+  return this._size;
+};
 
+Queue.prototype.enqueue = function (val) {
+  this._storage[this._size + this._head] = val;
+  this._size++;
+};
+
+Queue.prototype.dequeue = function () {
+  if (this._size === 0) {
+    return;
+  }
+  var temp = this._storage[this._head];
+  this._size--;
+  this._head++;
+  return temp;
+};
