e053bf2a5fdb02ddec8049725d87a7c44fabb63a
neillobo <neillobo@outlook.com>
2014-06-05 10:50:35 -0700

    Completed Linked Lists

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,51 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = makeNode(value);
+    //previous last element.next points to new element
+    if(list.tail !== null){
+      list.tail.next = newNode;
+    }
+    //head should point to first element
+    if (list.head === null) {
+      list.head = newNode;
+    }
+    //tail should point new element
+    list.tail = newNode;
   };
 
   list.removeHead = function(){
+
+    //if list is empty return nothing
+    if (list.head === null) {
+      return;
+    }
+    //otherwise store head temporarily
+    var temp = list.head;
+
+    //make head point to next element
+    list.head = list.head.next;
+
+
+    //return temp
+    return temp.value;
+
   };
 
   list.contains = function(target){
+    var found = false;
+    var current = list.head;
+
+    while (current!== null && !found){
+
+      if (current.value === target){
+        found = true;
+      }
+      current = current.next;
+
+    }
+
+    return found;
   };
 
   return list;
