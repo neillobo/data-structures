e975af8bec9d65fd0d14565d117221ab60ebdb6f
neillobo <neillobo@outlook.com>
2014-06-07 14:12:33 -0700

    Finished Graphs

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -42,18 +42,52 @@ Graph.prototype.getEdge = function(fromNode, toNode){
   return result;
 };
 
+//takes a pair of nodes and returns the locations in the
+//_edges array of their relationship
+Graph.prototype.getIndiciesOfEdge = function (nodeA,nodeB){
+  var results = [];
+  for(var i = 0; i < this._edges.length; i++){
+    if(this._edges[i][0] === nodeA && this._edges[i][1] === nodeB){
+      results.push(i);
+    }
+    if(this._edges[i][0] === nodeB && this._edges[i][1] === nodeA){
+      results.push(i);
+    }
+  }
+  return results;
+};
+
 Graph.prototype.addEdge = function(fromNode, toNode){
   this._edges.push([fromNode, toNode]);
   this._edges.push([toNode, fromNode]);
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
-  var fromTo = [fromNode, toNode];
-  var toFrom = [toNode, fromNode];
-  if (this.getEdge(fromNode, toNode)){
-    this._edges.splice(this._edges.indexOf(fromTo, 1));
-    this._edges.splice(this._edges.indexOf(toFrom, 1));
+  var indicies = this.getIndiciesOfEdge(fromNode, toNode);
+  debugger;
+  for (var i = indicies.length - 1; i >= 0; i--){
+    this._edges.splice(indicies[i], 1);
   }
+  var doesEdgeExist = function(node) {
+    var result = false;
+    //traverse adges array
+
+    for (var i = 0; i < this._edges.length; i++){
+      //check if i -0 or i - 1 === node
+      if (this._edges[i][0] === node ){
+        result = true;
+      }
+    }
+    return result;
+  }.bind(this);
+
+  if (!doesEdgeExist(fromNode)){
+    this.removeNode(fromNode);
+  }
+  if (!doesEdgeExist(toNode)){
+    this.removeNode(toNode);
+  }
+
 };
 
 /*
